9cb02a3050c560fc3afdc291257378ffb33ac89d
sndnvaps <sndnvaps@yahoo.com>
2015-12-17 23:55:18 +0800

    [wip][twrp]Add build option for mokee build twrp
    Change-Id: If6f7f33f9f1d493c985bc36b8a383db851cb9805
    Signed-off-by: sndnvaps <sndnvaps@yahoo.com>

--- Android.mk
+++ Android.mk
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+ifeq ($(BUILD_TWRP),)
 
 ifeq ($(call my-dir),$(call project-path-for,recovery))
 
@@ -340,4 +341,5 @@ include $(LOCAL_PATH)/minui/Android.mk \
     $(LOCAL_PATH)/applypatch/Android.mk \
     $(LOCAL_PATH)/fstools/Android.mk
 
+endif
 endif
